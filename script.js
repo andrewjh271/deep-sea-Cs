48d6e257f60d6364f04b45351a3ac4fb0629ac91
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2025-02-18 22:25:34 -0800

    Improve and clean up styling. Clean up code

--- script.js
+++ script.js
@@ -8,59 +8,51 @@ const blank = document.querySelector('.blank');
 
 const word = document.querySelector('.word');
 
-resetButton.addEventListener('click', hardReset);
-
-submit.addEventListener('click', start);
-
 const level1 = document.querySelector('.c');
 const level2 = document.querySelector('.ck');
 const level3 = document.querySelector('.k');
 
 const no1 = level1.querySelector('.no');
 const no2 = level2.querySelector('.no');
-const no3 = level3.querySelector('.no');
-
 const yes1 = level1.querySelector('.yes');
 const yes2 = level2.querySelector('.yes');
-const yes3 = level3.querySelector('.yes');
 
+submit.addEventListener('click', start);
+resetButton.addEventListener('click', hardReset);
 
+yes1.addEventListener('click', goToLevel2);
+no1.addEventListener('click', goToLevel1);
+no1.addEventListener('click', () => show('c'));
+yes2.addEventListener('click', () => show('ck'));
+yes2.addEventListener('click', goToLevel2);
+no2.addEventListener('click', () => setTimeout(() => show('k'), 1000));
+no2.addEventListener('click', gotoLevel3);
 
 function start() {
   reset();
   const [beg, end] = input.value.match(/\w+/g);
   part1.textContent = beg;
   part2.textContent = end;
+  goToLevel1();
+}
 
+function goToLevel1() {
   word.classList.add('level-1');
+  word.classList.remove('level-2');
+  word.classList.remove('level-3');
 }
-
-function adjustWord(length) {
-  if (length <= 4) {
-    word.classList.add('small');
-  } else if (length <= 7) {
-    word.classList.add('medium');
-  } else {
-    word.classList.add('large');
-  }
-}
-
-yes1.addEventListener('click', goToLevel2);
-no2.addEventListener('click', gotoLevel3);
 
 function goToLevel2() {
-  word.classList.remove('level-1');
   word.classList.add('level-2');
+  word.classList.remove('level-1');
+  word.classList.remove('level-3');
 }
 
 function gotoLevel3() {
-  word.classList.remove('level-2');
   word.classList.add('level-3');
-  show('k');
+  word.classList.remove('level-2');
+  word.classList.remove('level-1');
 }
-
-no1.addEventListener('click', () => show('c'));
-yes2.addEventListener('click', () => show('ck'));
 
 function show(char) {
   blank.textContent = char;
@@ -72,9 +64,6 @@ function reset() {
   word.classList.remove('level-1');
   word.classList.remove('level-2');
   word.classList.remove('level-3');
-  word.classList.remove('small');
-  word.classList.remove('medium');
-  word.classList.remove('large');
 }
 
 function hardReset() {
